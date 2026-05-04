9546dc8cb8efcc734b639827af2b5fd4a63c30fa
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-05-04 22:38:59 +0600

    instence ts

---
+++ src/instance.ts
@@ -0,0 +1,42 @@
+class Person {
+    name: string;
+    constructor(name: string){
+        this.name = name
+    }
+
+    getSleep(numOfHours:number){
+        console.log(`${this.name} doinik ${numOfHours} ghumai`);
+        
+    }
+}
+
+class Student extends Person {
+    constructor(name:string){
+        super(name)
+    }
+    doStudy(numOfHours:number) {
+        console.log(`${this.name} doinik ${numOfHours} study kore`);
+    }
+}
+class Teacher extends Person {
+    constructor(name:string){
+        super(name)
+    }
+    takeClass(numOfHours:number) {
+        console.log(`${this.name} doinik ${numOfHours} class nai`);
+    }
+}
+const getUserInfo = (user: Person) => {
+    if(user instanceof Student){
+        user.doStudy(8)
+    }else if(user instanceof Teacher){
+        user.takeClass(6)
+    }else{
+        user.getSleep(12)
+    }
+}
+const student1 = new Student("Mr. Student")
+const teacher1 = new Teacher("Mr. Teacher")
+const man1 = new Person("Mr. Man")
+
+getUserInfo(man1)
